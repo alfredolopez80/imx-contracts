83518c29055b8f25da97deb4e8edd737472e75d8
alfredolopez80 <alfredolopez80@gmail.com>
2021-11-26 17:36:25 -0400

    ADD: setup dev environment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
+import 'hardhat-typechain'
+import '@typechain/ethers-v5'
 import "@nomiclabs/hardhat-etherscan";
 import dotenv from "dotenv";
 
@@ -12,17 +13,17 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     dev: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.INFURE_API_KEY}`,
       accounts: [`0x${process.env.DEPLOYER_ROPSTEN_PRIVATE_KEY}`],
     },
     ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      url: `https://ropsten.infura.io/v3/${process.env.INFURE_API_KEY}`,
       accounts: [`0x${process.env.DEPLOYER_ROPSTEN_PRIVATE_KEY}`],
     },
-    mainnet: {
-      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [`0x${process.env.DEPLOYER_MAINNET_PRIVATE_KEY}`],
-    },
+    // mainnet: {
+    //   url: `https://mainnet.infura.io/v3/${process.env.INFURE_API_KEY}`,
+    //   accounts: [`0x${process.env.DEPLOYER_MAINNET_PRIVATE_KEY}`],
+    // },
   },
   typechain: {
     outDir: "artifacts/typechain",
